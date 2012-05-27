9e5d3972e2f3c9746899de7b104d9372473d58a2
Maik Schünemann <maikschuenemann@gmx.de>
2012-05-27 13:30:56 +0200

    test

---
+++ src/PAIP2clojure/core.clj
@@ -0,0 +1,1 @@
+(ns PAIP2clojure.core)
---
+++ test/PAIP2clojure/test/core.clj
@@ -0,0 +1,6 @@
+(ns PAIP2clojure.test.core
+  (:use [PAIP2clojure.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
