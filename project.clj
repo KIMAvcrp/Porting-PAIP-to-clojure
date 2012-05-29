e60c3679e89b83230ca3961889ffffcc3c08065b
Maik Schünemann <maikschuenemann@gmx.de>
2012-05-29 17:30:15 +0200

    moved src in the right place

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject PAIP2clojure "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.trace "0.7.3"]])
